404426221d42601fc55dcd1a41b80caa9ff4bb92
Derek Greentree <derek@lijit.com>
2010-05-07 22:04:02 -0600

    WIP

--- src/clojure/cascading_clj.clj
+++ src/clojure/cascading_clj.clj
@@ -1,48 +1,37 @@
-(ns cascading-clj
-  (:require [cascading-clj.imports :as imp])
-  (:import (java.util Properties))
-  (:use (cascading-clj io tuples)))
+(ns cascading-clj-2)
 
-(imp/import-all)
+(defmacro cfn [init-expr & body]
+  "Read a str'ed clojure value, expect init-expr to be a binding form. Bind
+  the value to the init-expr. Execute body, and prnstr the resultant value."
+  `(fn [s#] 
+     (when-let [result#
+                (let [obj# (read-string s#)]
+                  (apply (fn [~@init-expr] ~@body) obj#))]
+       (pr-str result#))))
 
-(defmacro gen-flow []
-  "Generates a class in the calling namespace that proxies a main[] method. The method's name should be tool-main. The assemble* family
-  of functions generate tool-main when called."
-  (let [the-name (.replace (str (ns-name *ns*)) \- \_)]
-    `(gen-class
-      :name ~the-name
-      :prefix "tool-"
-      :main true)))
+(defmacro defcfn [nm init-expr &body]
+  "Convenience for naming a cfn'ed body."
+  `(def ~nm (cfn ~init-expr ~@body)))
 
-(defn assemble*
-  "Given a pipe description, a bag of java.util.Properties, a function yielding a Source from a given input path,
-  one yielding a sink from a given input path, a sequence of chained assembly operations, and a function taking a connected Flow,
-  create a pipeline from the source sink, through the chain, to the target sink, and apply flow-fn to it."
-  ([desc props sourcefn sinkfn pipes flowfn] 
-   (let [the-name (.replace (str (ns-name *ns*)) \- \_)]
-     (intern *ns* 'tool-main
-             (fn [& args]
-               (FlowConnector/setApplicationJarClass props (Class/forName the-name))
-               (let [flow (FlowConnector. props)
-                     pipe (Pipe. desc)
-                     chained (pipes pipe)]
-                 (flowfn (.connect flow desc (sourcefn (first args)) (sinkfn (last args)) chained)))))))
-  ([desc sourcefn sinkfn pipes flowfn] (assemble* desc (Properties.) sourcefn sinkfn flowfn pipes)))
+(defn- unpack-tuple [t]
+  (.. t getArguments getTuple (getString t 0)))
 
-(defn assemble-props
-  "Adds a standard main method, named tool-main, to the current
-  namespace, that will execute the flow-fn on the given flow when run."
-  ([desc outputs props pipes flow-fn]
-   (let [source #(hdfs-source %)
-         sink #(hdfs-delimited-sink % (fields outputs))
-         jobconf (doto (Properties.) (.putAll props))]
-     (assemble* desc jobconf source sink flow-fn pipes)))
-  ([desc outputs props pipes] 
-   (assemble-props desc outputs props pipes (memfn complete))))
+(defn- pack-tuple [s]
+  (Tuple. s))
 
-(defn assemble
-  "Adds a standard main method, named tool-main, to the current
-  namespace, that will execute the flow-fn on the given flow when run."
-  ([desc outputs pipes flow-fn]
-   (assemble-props desc outputs {} pipes flow-fn))
-   ([desc outputs pipes] (assemble desc outputs pipes (memfn complete))))
+(defn each-cfn [f]
+  "Wraps the single cfn'ed expression around tuple reading and writing, and implementing
+  the proxy interface."
+  (proxy [BaseOperation Function] []
+    (operate [flow call] 
+             (let [argstr (unpack-tuple call)]
+               (when-let [result (f argstr)]
+                   (let [result-tuple (pack-tuple result)
+                         out (.getOutputCollector call)]
+                     (.add out result-tuple)))))))
+
+(defn chain [lhs fs]
+  "Takes a sequence of cfn's and returns a function that takes the head of a pipe assembly,
+  and returns the result of applying Each function to it, which is another pipe assembly."
+  (let [pipeline (reduce (fn [pipe f] (Each. #^Pipe pipe Fields/FIRST (each-cfn f))) lhs fs)]
+    (cap (importer pipeline))))
